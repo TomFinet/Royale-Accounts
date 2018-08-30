f0f47c4615131bfd2ba259f1b9788824113a3e0c
Tom Finet <tomfinet@toms-macbook-air-2.home>
2018-08-30 14:39:02 +0200

    Removed syntax error

--- source/users/urls.py
+++ source/users/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LogoutView
 from .views import (
-	login_page, 
-	register_page, 
-	guest_register_view, 
+	login_page,
+	register_page,
+	guest_register_view,
 	email_form_page,
 	email_resend_view,
 )
@@ -17,7 +17,7 @@ urlpatterns = [
 	url(r'^register-guest/', guest_register_view, name="guest_register"),
 
 	url(r'^password-reset-email/$', email_form_page, name="password_reset_email"),
-	url(r'^password-reset/resend/$', email_resend_view, name="password_reset_resend_email")
+	url(r'^password-reset/resend/$', email_resend_view, name="password_reset_resend_email"),
 	#url(r'^password-reset/(?P<token>[0-9A-Za-z]{1,16})/(?P<user_id>[\d+]+)$',
 		#.as_view(), name='activate_account'),
 ]
