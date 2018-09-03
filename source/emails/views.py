35c86e93bce5a8719da473323658b79be68510e3
Tom Finet <tomfinet@toms-macbook-air-2.home>
2018-09-03 14:05:41 +0200

    Fixed error in email view confirmation email

--- source/emails/views.py
+++ source/emails/views.py
@@ -4,11 +4,15 @@ from __future__ import unicode_literals
 from django.shortcuts import render, redirect
 from django.template.loader import get_template
 from django.conf import settings
+from django.contrib.auth import get_user_model
+
+from users.models import AccessToken
 
 import sendgrid
 from sendgrid.helpers.mail import *
 
 
+User = get_user_model()
 def order_confirmation_email_view(request):
 
 	# get session variables
@@ -17,6 +21,9 @@ def order_confirmation_email_view(request):
 	accounts = request.session.get("accounts", None)
 
 	if to_email and order_id and accounts:
+
+		user = User.objects.filter(email=to_email)
+		token, created = AccessToken.objects.new_or_get(user)
 
 		reset_link = getattr(settings, 'WEBSITE_URL') + "user/password-reset/" + token.token
 
