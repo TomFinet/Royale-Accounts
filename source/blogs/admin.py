57c0b227edbf58be4dac00bb6d9224a5b3240c8b
Tom Finet <tomfinet@toms-macbook-air-2.home>
2018-09-03 16:45:44 +0200

    Added blog model to the admin

--- source/blogs/admin.py
+++ source/blogs/admin.py
@@ -3,4 +3,21 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, PostImage
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ['__str__', 'slug']
+
+	class Meta:
+		model = Post
+
+admin.site.register(Post, PostAdmin)
+
+
+class PostImageAdmin(admin.ModelAdmin):
+	list_display = ['__str__']
+
+	class Meta:
+		model = PostImage
+
+admin.site.register(PostImage, PostImageAdmin)
