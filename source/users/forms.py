ae10e0cc372df2b6c491c196b81588d7e7459731
Tom Finet <tomfinet@toms-macbook-air-2.home>
2018-09-04 13:28:38 +0200

    Modified details template

--- source/users/forms.py
+++ source/users/forms.py
@@ -44,7 +44,7 @@ class RegisterForm(forms.ModelForm):
         # Save the provided password in hashed format
         user = super(RegisterForm, self).save(commit=False)
         user.set_password(self.cleaned_data["password1"])
-        user.active = False
+        user.active = True # change to false with account verification
         user.save()
         return user
 
