a897cff5c8bc4fc9cca68669b22eb217fd262ec5
Tom Finet <tomfinet@toms-macbook-air-2.home>
2018-09-05 11:58:08 +0200

    Changed url pattern for pass reset

--- source/users/urls.py
+++ source/users/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
 
 	url(r'^password-reset-email/$', email_form_page, name="password_reset_email"),
 	url(r'^password-reset/resend/$', email_resend_view, name="password_reset_resend_email"),
-	url(r'^password-reset/(?P<token>[0-9A-Za-z]{1,16})/$', 
+	url(r'^password-reset/(?P<token>[\w-]+)/$', 
 		reset_password_view, name='password_reset'),
 	url(r'^change-password/$', change_password_view, name="change_password"),
 ]
