a9053a8a26a2f36dfac1a3acf0ca65040f3f2218
Tom Finet <tomfinet@toms-macbook-air-2.home>
2018-09-05 22:10:14 +0200

    Added contact us page

--- source/royaleaccounts/forms.py
+++ source/royaleaccounts/forms.py
@@ -3,6 +3,5 @@ from django import forms
 
 from django.conf import settings
 
-
 class CurrencyForm(forms.Form):
 	currency = forms.ChoiceField(label="", choices=getattr(settings, "CURRENCY_CHOICES"), initial="USD")
