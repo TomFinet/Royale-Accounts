3435ae9be98d0f11f007e530062f45fb641b82d3
Tom Finet <tomfinet@toms-macbook-air-2.home>
2018-09-15 00:26:58 +0200

    Made debug = True

--- source/royaleaccounts/settings/production.py
+++ source/royaleaccounts/settings/production.py
@@ -24,9 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.royale-accounts.com']
 
